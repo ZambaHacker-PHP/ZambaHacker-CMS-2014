7f1bfceed34109d941f561f90ad8cdb932a4b856
zambahacker <zambahacker@hotmail.it>
2014-05-14 18:09:05 +0200

    Aggiunto readme

---
+++ cache/04/a5/926acbe9ce5bb2adf4f0a7fc32841a139b9c9784f0d41ee43b93a8e52c71.php
@@ -0,0 +1,56 @@
+<?php
+
+/* footer.twig */
+class __TwigTemplate_04a5926acbe9ce5bb2adf4f0a7fc32841a139b9c9784f0d41ee43b93a8e52c71 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        echo "\t<footer>
+\t\t\t<section id=\"header\" onclick=\"\"></section>
+\t\t\t<section id=\"content\">
+\t\t\t\t<article class=\"left\">
+\t\t\t\t\t<aside id=\"copyright\">
+\t\t\t\t\t\t<figure class=\"flink\">
+\t\t\t\t\t\t\tProgrammed by ZambaHacker<br />
+\t\t\t\t\t\t</figure>
+
+\t\t\t\t\t\t<figure class=\"basshunter\">
+\t\t\t\t\t\t\tDesigned by ZambaHacker<br />
+\t\t\t\t\t\t
+\t\t\t\t\t\t</figure>
+\t\t\t\t\t</aside>
+\t\t\t\t</article>
+
+\t\t\t\t<article class=\"middle\">
+\t\t\t\t\t&copy;  2014 - Questo sito web non f&agrave; parte e non &egrave; gestito da Sulake Group O.I<br />
+\t\t\t\tOgni diritto di questo CMS &egrave; riconducibile al rispettivo proprietario\" ZambaHacker \"
+\t\t\t\t\t<p>
+\t\t\t\t\tQuesto CMS &egrave; Sottoposto a Copyright <a href=\"http://creativecommons.org/licenses/by-nc-nd/3.0/deed.it\">Creative Commons</a>
+\t\t\t\t\t</p>
+\t\t\t\t</article>
+
+\t\t\t</section>
+\t\t</footer>";
+    }
+
+    public function getTemplateName()
+    {
+        return "footer.twig";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  292 => 144,  290 => 143,  258 => 130,  253 => 129,  233 => 128,  224 => 123,  163 => 81,  136 => 73,  104 => 45,  88 => 33,  83 => 32,  58 => 14,  53 => 13,  46 => 10,  41 => 9,  36 => 8,  31 => 7,  25 => 5,  19 => 1,);
+    }
+}
